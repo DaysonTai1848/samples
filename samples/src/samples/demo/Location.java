e3875eecfef4f7aec9987c4738df848841ff87b4
Dayson Tai <daysontai69@gmail.com>
2022-03-10 14:44:22 +0800

    Initialize location class

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	private int length;
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+}
